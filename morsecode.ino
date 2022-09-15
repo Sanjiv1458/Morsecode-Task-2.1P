9b2f72ceb0dfcd531d1cd65e979e89c428fc65d4
Sanjiv1458 <107008940+Sanjiv1458@users.noreply.github.com>
2022-09-15 15:02:25 +0530

    Update morsecode.ino

--- morsecode.ino
+++ morsecode.ino
@@ -16,7 +16,6 @@ void dash() {
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
 }
 
 // the loop function runs over and over again forever
