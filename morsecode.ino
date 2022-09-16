8205d5d683a44f7aa48faf46afcc55a9cc5ae185
Sanjiv1458 <107008940+Sanjiv1458@users.noreply.github.com>
2022-09-16 22:55:31 +0530

    Update morsecode.ino

--- morsecode.ino
+++ morsecode.ino
@@ -8,7 +8,7 @@ void dot() {
 }
 void dash() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for 2 second
+  delay(3000);                       // wait for 3 second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);
 }
